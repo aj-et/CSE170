766e95c1b9c3861c202ad7028d23da46c3fd29c8
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2022-10-29 23:17:39 -0600

    Added W07 stuff

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+setTimeout(() => {
+    const hide = document.getElementsByClassName("P06 R07");
+    hide.style.display = "none";
+}, 1);
+
+// function showFile() {
+//     var x = document.getElementById('R07');
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     }
+//     else {
+//         x.style.display = "none";
+//     }
+// }
